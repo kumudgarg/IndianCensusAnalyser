f7013d732a6178b6ad2934ff7ff4e1f138197ddb
kumudgarg <kumud50garg@gmail.com>
2019-12-09 13:52:52 +0530

    Refactored code to encapsulate all changes with in the csvBuilder

--- CensusAnalyser/src/main/java/censusanalyser/ICSVBuilder.java
+++ CensusAnalyser/src/main/java/censusanalyser/ICSVBuilder.java
@@ -4,7 +4,7 @@ import java.io.Reader;
 import java.util.Iterator;
 
 public interface ICSVBuilder {
-    public  <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException;
+    public  <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException;
 
 
 }
