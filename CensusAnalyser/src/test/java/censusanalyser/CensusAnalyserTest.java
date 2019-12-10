6ab4de6e42b0d3ed195cfbe0e04d79e5ce01b648
kumudgarg <kumud50garg@gmail.com>
2019-12-10 17:32:12 +0530

    Testing by giving wrong delimiter such that would it be graped custom exception type or not

--- CensusAnalyser/src/test/java/censusanalyser/CensusAnalyserTest.java
+++ CensusAnalyser/src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -10,6 +10,7 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String INDIAN_CSV_STATE_PATH = "./src/test/resources/IndiaStateCode.csv";
+    private static final String INDIAN_CENSUS_CSV_WRONG_DELIMITER = "./src/test/resources/IndiaStateCensusDataWrongDelimiter.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -18,6 +19,16 @@ public class CensusAnalyserTest {
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29, numOfRecords);
         } catch (CensusAnalyserException e) {
+        }
+    }
+
+    @Test
+    public void givenWrongDelimiter_InIndiaCensusData_ShouldReturnCustomExceptionType() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_CSV_WRONG_DELIMITER);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_DELIMITER_PROBLEM, e.type);
         }
     }
 
