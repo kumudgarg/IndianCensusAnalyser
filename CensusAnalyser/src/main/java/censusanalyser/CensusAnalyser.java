b44628860e0fbdf78432a60332f18657ff7e0b93
kumudgarg <kumud50garg@gmail.com>
2019-12-10 18:10:46 +0530

    Refactored code by adding a extra catch clause for null pointer exception

--- CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
+++ CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
@@ -33,10 +33,15 @@ public class CensusAnalyser {
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         } catch (CSVBuilderException e) {
             throw new CensusAnalyserException(e.getMessage(), e.type.name());
-        } catch (RuntimeException e) {
+        }  catch (NullPointerException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
+        }
+        catch (RuntimeException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES);
         }
+
     }
 
     public int loadStateCode(String indiaCensusCSVFilePath) throws CensusAnalyserException {
